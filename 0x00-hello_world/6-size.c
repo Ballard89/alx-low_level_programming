b4f88c84d85c5995614aa1fab4709a91f1c3203c
ballard89 <tosinadeleye66@gmail.com>
2022-09-11 07:27:05 -0700

    size of type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main- is the entry point for the program.
+ * main -is the entry point for the program.
  *
- * Return: zero if no error,non zero if error.
+ * Return:zero if no errors,non-zero value if errors.
  */
 int main(void)
 {
